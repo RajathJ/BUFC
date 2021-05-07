7cc3d94e44ac385a0ef2db2bbcf0e6961cac5c53
RajathJ <rajathj2000@gmail.com>
2021-05-07 20:31:58 +0530

    changes

--- bufc/feedback_form.php
+++ bufc/feedback_form.php
@@ -1,43 +1,16 @@
 <?php
-#Receive user input
+//get data from form  
 $name = $_POST['name'];
-$email_address = $_POST['email_address'];
+$email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-
-#Filter user input
-function filter_email_header($form_field) {  
-return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+$to = "rajathj2000@.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
-
-$email_address  = filter_email_header($email_address);
-
-#Send email
-$headers = "From: $email_address";
-$sent = mail('rajathj2000@gmail.com', 'Feedback Form Submission', $name,$subject,$message, $headers);
-
-#Thank user or notify them of a problem
-if ($sent) {
-?><html>
-<head>
-<title>Thank You</title>
-</head>
-<body>
-<h1>Thank You</h1>
-<p>Thank you for your feedback.</p>
-</body>
-</html>
-<?php
-} else {
-?><html>
-<head>
-<title>Something went wrong</title>
-</head>
-<body>
-<h1>Something went wrong</h1>
-<p>We could not send your feedback. Please try again.</p>
-</body>
-</html>
-<?php
-}
+//redirect
 ?>
